ca7f90f453cd5081aedbce515c7527fba38e5d89
Sam <solusany@students.kennesaw.edu>
2021-11-12 23:31:36 -0500

    Gesture Library
    tkintering with tkinter

--- program/GestureLibrary.py
+++ program/GestureLibrary.py
@@ -1,1 +1,19 @@
 ''' code for main program '''
+import tkinter as tk
+from tkinter import *
+from menu import menu
+
+# create overall window with title
+root = tk.Tk()
+root.geometry("800x700")
+root['bg']='black'
+root.title("GestureLibrary")
+root.iconphoto = ("")
+topFrame = Frame(root)
+topFrame.place(relx=0,relheight=1,relwidth=1)
+mainMenu = menu(topFrame)
+mainMenu.place(relx=0,relheight=1,relwidth=1)
+
+
+# display the window
+root.mainloop()
--- program/menu.py
+++ program/menu.py
@@ -1,1 +1,13 @@
 ''' code for main menu '''
+from tkinter import *
+
+class menu(Frame):
+
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.topFrame = parent
+        # hold text within the previous root instance and with horizontal & vertical padding
+        label = Label(text="Gesture Library", fg='Gold', font=("Helvetica",40), padx=100, pady=50)
+        label.pack()        
+        self.btClose = Button(self,text="Exit",height=3,width=15,command=self.quit)
+        self.btClose.place(anchor="center",relx=0.5,rely=0.5,y=270)
