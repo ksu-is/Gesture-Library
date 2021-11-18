5288c91dc996c6ce54c84e4c9fb1205d2ce4729d
Sam <solusany@students.kennesaw.edu>
2021-11-18 16:22:00 -0500

    Update GestureLibrary.py

--- program/GestureLibrary.py
+++ program/GestureLibrary.py
@@ -1,22 +1,4 @@
 ''' code for main program '''
-# import tkinter as tk
-# from tkinter import *
-# from menu import menu
-
-# # create overall window with title
-# root = tk.Tk()
-# root.geometry("800x700")
-# root['bg']='black'
-# root.title("GestureLibrary")
-# root.iconphoto = ("")
-# topFrame = Frame(root)
-# topFrame.place(relx=0,relheight=1,relwidth=1)
-# mainMenu = menu(topFrame)
-# mainMenu.place(relx=0,relheight=1,relwidth=1)
-
-
-# # display the window
-# root.mainloop()
 
 from tkinter import *
 
@@ -28,7 +10,7 @@ def startSession(tu):
     if(imgMan.getImageLen()>0):
         imgMan.randomizeImages()
         mainMenu.place_forget()
-        #mainMenu.place(relx=0,relheight=1,relwidth=1)
+        
         gestureScreen.setSession(tu)
         gestureScreen.place(relx=0,relheight=1,relwidth=1)
 def exitSession():
@@ -42,18 +24,14 @@ root.title("GestureLibrary")
 root.iconphoto = ("")
 topFrame = Frame(root)
 imgMan.loadFolders()
-#imgManager = ImageManager()
-#imgManager.loadFolders()
-#help(os)
-#topFrame.pack(side="top",fill="both",expand=True)
+
 topFrame.place(relx=0,relheight=1,relwidth=1)
 mainMenu = MainMenu(topFrame)
 mainMenu.place(relx=0,relheight=1,relwidth=1)
 mainMenu.setFolders()
-#mainMenu.bindUpdateImageCount(imgManager.updateImageCount)
+
 mainMenu.bindStartSession(startSession)
 gestureScreen = GestureScreen(topFrame)
 gestureScreen.bindExitSession(exitSession)
-#gestureScreen.place(relx=0,relheight=1,relwidth=1)
-#root.bind("<ButtonPress>",teste)
+
 root.mainloop()
